92d3708a661dd2301643d98bd42f9699b9cd65f3
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2018-04-13 09:42:51 +0200

    Titulo

---
+++ src/main/java/MavenDocumentacion/Metodos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MavenDocumentacion;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Metodos {
+   
+    //Metodo para sacar la ciudad, el pais, y el titulo de la pagina
+   
+    public static void Titulo(){
+        
+        try{
+            
+            Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+            String title = doc.title();
+            System.out.println("Datos extraidos de "+title);
+            
+        }catch(IOException ex){
+            System.out.println("Error al sacar el titulo:");
+        }
+    }
+
+}
