6a630869a817fd603b635d68449e909173995c1f
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2018-04-13 09:42:44 +0200

    Titulo

--- src/main/java/MavenDocumentacion/Main.java
+++ src/main/java/MavenDocumentacion/Main.java
@@ -5,6 +5,8 @@
  */
 package MavenDocumentacion;
 
+import static MavenDocumentacion.Metodos.*;
+
 /**
  *
  * @author ggarciaalvarez
@@ -16,7 +18,19 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        
+//        Realiza una aplicación que muestre por pantalla el estado del cielo y la temperatura en Vigo de una página web.
+//
+//        Ejemplo de salida del programa: "Hoy está nublado y hace 14º. Datos extraídos de Vigo, "
+//                + "España Pronóstico del tiempo y condiciones meteorológicas - "
+//                + "The Weather Channel | Weather.com"
+
+
+
+    //Aqui unicamente debemos llamar a los metodos
+    Titulo();
+
+    
+
     }
     
 }
